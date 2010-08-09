648403a3a89914497ce14c1a8edd3784c0934b84
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2010-08-09 01:33:39 +0000

    insert in place, should work without a problem. cleaned up some unused old cold.  starting to comment each line for remembering
    git-svn-id: http://masterpiece.googlecode.com/svn@48 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -59,18 +59,6 @@ MainWindow::MainWindow(void)
 	}
 }
 
-/*
-static int ReturnCount(void *NotUsed, int resultCount, char **colResults, char **colName)
-{
-	int i;
-	for(i=0;i<resultCount;i++)
-	{
-		fprintf(stdout, "count: %d, %s = %s\n", resultCount, colName[i], colResults[i]);
-	}
-	return 0;
-} 
-*/
-
 void MainWindow::MessageReceived(BMessage *msg)
 {
 	
@@ -126,68 +114,25 @@ void MainWindow::MessageReceived(BMessage *msg)
 					else if(alertReturn == 1) // Create
 					{
 						fprintf(stdout, "create course\n");
+						tmpString = "insert into mptable (masterpieceName) values('";
+						tmpString += this->fullView->titleText->Text();
+						tmpString += "');";
+						sqlValue = sqlite3_exec(mpdb, tmpString, ReturnCount, NULL, &sqlErrMsg);
+						this->SetTitle(this->fullView->titleText->Text());
+						if(!this->fullView->IsHidden()) this->fullView->Hide();
+						if(!this->openView->IsHidden()) this->openView->Hide();
+						if(this->sumView->IsHidden()) this->sumView->Show();
+						this->mpMenuBar->contentMenu->SetEnabled(true);
+						this->mpMenuBar->layoutMenu->SetEnabled(true);			
 					}
 					else if(alertReturn == 2) // Cancel
 					{
 						fprintf(stdout, "cancel course\n");
 					}
-			/*
-					need to call insert statement if the user want's to create it based off of
-					the alert dialog
-					tmpString = "insert into mptable (masterpieceName) values('";
-					tmpString += this->fullView->titleText->Text();
-					tmpString += "');";
-			*/
-					//sqlValue = sqlite3_exec(mpdb, tmpString, ReturnCount, NULL, &sqlErrMsg);
 				}
 			}
+			this->fullView->titleText->SetText(""); // reset new course title to blank when done
 			fprintf(stdout, "count check errors: %s\n", sqlErrMsg);
-			// frpintf(stdout, "count value check: %d\n", 
-			// fprintf(stdout, "insert: %s", sqlErrMsg);
-			// possibly check result before doing the following:
-			this->SetTitle(this->fullView->titleText->Text());
-			if(!this->fullView->IsHidden()) this->fullView->Hide();
-			if(!this->openView->IsHidden()) this->openView->Hide();
-			if(this->sumView->IsHidden()) this->sumView->Show();
-			this->mpMenuBar->contentMenu->SetEnabled(true);
-			this->mpMenuBar->layoutMenu->SetEnabled(true);			
-			
-			/*
-			returnValue = homeDir->CreateDirectory(this->fullView->titleText->Text(), homeDir);
-			if(returnValue == B_FILE_EXISTS)
-			{
-				tmpString = "Do you want to open the course ";
-				tmpString += this->fullView->titleText->Text();
-				tmpString += "?";
-			}
-			if(returnValue != B_OK)
-			{
-				debugAlert = new BAlert("Debug Value", tmpString, "Yes", "No", 0, B_WIDTH_AS_USUAL, B_INFO_ALERT);
-				debugAlert->MoveTo(350, 250);
-				debugAlert->SetShortcut(1, B_ESCAPE);
-				int alertReturn = debugAlert->Go();
-				if(alertReturn == 0) // Yes
-				{
-					this->SetTitle(this->fullView->titleText->Text());
-					if(!this->fullView->IsHidden()) this->fullView->Hide();
-				}
-				else if(alertReturn == 1) // No
-				{
-					this->fullView->titleText->SetText("");
-				}
-			}
-			else
-			{
-				this->SetTitle(this->fullView->titleText->Text());
-				if(!this->fullView->IsHidden()) this->fullView->Hide();
-				if(!this->openView->IsHidden()) this->openView->Hide();
-				if(this->sumView->IsHidden()) this->sumView->Show();
-				this->mpMenuBar->contentMenu->SetEnabled(true);
-				this->mpMenuBar->layoutMenu->SetEnabled(true);
-			}
-			// do something here...
-			*/
-			this->fullView->titleText->SetText("");
 			break;
 		
 		case CANCEL_NEW_COURSE:		
