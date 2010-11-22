192e2866160180c215f8c504ef5f245488e426cc
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2010-11-22 01:17:07 +0000

    working on adding bcardlayout instead of bgridlayout.  also need to add bboxes to segregate areas.
    git-svn-id: http://masterpiece.googlecode.com/svn@130 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -229,3 +229,53 @@ void MainWindow::PopulateSummaryView(int mpID)
 	//tmpString = "select thoughtID, thoughtData from ttable wher
 	// place code here to populate summary view based on input id...
 }
+/*
+#ifndef	_CARD_LAYOUT_H
+#define	_CARD_LAYOUT_H
+
+#include <AbstractLayout.h>
+
+
+class BCardLayout : public BAbstractLayout {
+public:
+								BCardLayout();
+								BCardLayout(BMessage* from);
+	virtual						~BCardLayout();
+
+			BLayoutItem*		VisibleItem() const;
+			int32				VisibleIndex() const;
+			void				SetVisibleItem(int32 index);
+			void				SetVisibleItem(BLayoutItem* item);
+
+	virtual	BSize				BaseMinSize();
+	virtual	BSize				BaseMaxSize();
+	virtual	BSize				BasePreferredSize();
+	virtual	BAlignment			BaseAlignment();
+
+	virtual	bool				HasHeightForWidth();
+	virtual	void				GetHeightForWidth(float width, float* min,
+									float* max, float* preferred);
+
+	virtual	void				InvalidateLayout(bool children = false);
+
+	virtual status_t			Archive(BMessage* into, bool deep = true) const;
+	virtual status_t			AllUnarchived(const BMessage* from);
+	static	BArchivable*		Instantiate(BMessage* from);
+
+protected:
+	virtual	void				DerivedLayoutItems();
+	virtual	bool				ItemAdded(BLayoutItem* item, int32 atIndex);
+	virtual	void				ItemRemoved(BLayoutItem* item, int32 fromIndex);
+
+private:
+			BSize				fMin;
+			BSize				fMax;
+			BSize				fPreferred;
+			BLayoutItem*		fVisibleItem;
+			bool				fMinMaxValid;
+
+			void				_ValidateMinMax();
+};
+
+#endif	// _CARD_LAYOUT_H
+*/
