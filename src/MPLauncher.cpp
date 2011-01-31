b82b21cbadde84f3e1c8a424bc7dbedd3f8a767e
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-01-31 02:08:23 +0000

    need to switch layout to layout builder as well as left align a bunch of stuff with insets and what not and glue...
    git-svn-id: http://masterpiece.googlecode.com/svn@174 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/MPLauncher.cpp
+++ src/MPLauncher.cpp
@@ -1,7 +1,7 @@
 #include "MPLauncher.h"
 
 MPLauncher::MPLauncher(void)
-	:	BWindow(BRect(100, 100, 450, 300), "MasterPiece Launcher", B_DOCUMENT_WINDOW,  B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
+	:	BWindow(BRect(100, 100, 450, 300), "MasterPiece Launcher", B_TITLED_WINDOW,  B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
 {
 	mainGrid = new BGridLayout();
 	newThoughtButton = new BButton(BRect(10, 10, 90, 35), NULL, "Create a New...", new BMessage(CREATE_NEW_THT), B_FOLLOW_NONE, B_WILL_DRAW);
