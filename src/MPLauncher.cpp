0e7309a3ce302085629c420ca8d67edbdcefb814
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-01-31 01:16:05 +0000

    working on new launcher window
    git-svn-id: http://masterpiece.googlecode.com/svn@171 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/MPLauncher.cpp
+++ src/MPLauncher.cpp
@@ -1,2 +1,14 @@
 #include "MPLauncher.h"
 
+MPLauncher::MPLauncher(void)
+	:	BWindow(BRect(100, 100, 450, 300), "MasterPiece Launcher", B_DOCUMENT_WINDOW,  B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
+{
+	mainGrid = new BGridLayout();
+	newThoughtButton = new BButton(BRect(10, 10, 90, 35), NULL, "Create a New", new BMessage(CREATE_NEW_THT), B_FOLLOW_NONE, B_WILL_DRAW);
+	newMasterpieceButton = new BButton(BRect, 10, 10, 90, 35), NULL, "Create a New", new BMessage(CREATE_NEW_MP), B_FOLLOW_NONE, B_WILL_DRAW);
+	
+	backView = new BView(Bounds(), "backview", B_FOLLOW_ALL, B_WILL_DRAW);
+	backView->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
+	AddChild(backView);
+	
+}
--- src/MPLauncher.h
+++ src/MPLauncher.h
@@ -5,15 +5,28 @@
 #include <View.h>
 #include <sqlite3.h>
 #include <GridLayout.h>
+#include <Button.h>
+#include <ListView.h>
+#include <ScrollView.h>
+#include <StringView.h>
 
 class MPLauncher : public BWindow
 {
 public:
-				MPLauncher(void);
-		void	MessageReceived(BMessage* msg);
+						MPLauncher(void);
+		void			MessageReceived(BMessage* msg);
 		
 private:
 	
+		BView*			backView;
+		BGridLayout*	mainGrid;
+		BButton*		newMasterpieceButton;
+		BButton*		newThoughtButton;
+		BListView*		openThoughtListView;
+		BListView*		openMasterpieceListView;
+		BStringView*	thoughtStringView;
+		BStringView*	masterpieceStringView;
+		
 };
 
 #endif
