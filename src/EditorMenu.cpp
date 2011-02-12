f193cba80b9670418217900235b8dc95374f6950
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-02-12 00:34:36 +0000

    editormenu is ready for adding to the editorwindow
    git-svn-id: http://masterpiece.googlecode.com/svn@212 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/EditorMenu.cpp
+++ src/EditorMenu.cpp
@@ -1,2 +1,34 @@
 #include "EditorMenu.h"
 
+EditorMenu::EditorMenu()
+	:	BMenuBar(BRect(0, 0, 10, 10), "editorbar")
+{
+	// initialize menu and menuitems
+	helpMenu = new BMenu("Help");
+	newThoughtMenuItem = new BMenuItem("New Thought", new BMessage(MENU_NEW_THT));
+	editThoughtMenuItem = new BMenuItem("Edit Thought Name", new BMessage(MENU_EDT_THT));
+	saveThoughtMenuItem = new BMenuItem("Save Progress", new  BMessage(MENU_SAV_THT));
+	previewMenuItem = new BMenuItem("Preview", new BMessage(MENU_PRV_THT));
+	publishMenuItem = new BMenuItem("Publish", new BMessage(MENU_PUB_THT));
+	keyboardRefMenuItem = new BMenuItem("Keyboard Reference", new BMessage(MENU_KEY_THT));
+	markupRefMenuItem = new BMenuItem("Markup Reference", new BMessage(MENU_MRK_THT));
+	helpTopicsMenuItem = new BMenuItem("Help Topics", new BMessage(MENU_HLP_THT));
+	aboutMenuItem = new BMenuItem("About", new BMessage(MENU_ABT_THT));
+	
+	//layout helpMenu
+	helpMenu->AddItem(markupRefMenuItem);
+	helpMenu->AddItem(keyboardRefMenuItem);
+	helpMenu->AddItem(helpTopicsMenuItem);
+	helpMenu->AddSeparatorItem();
+	helpMenu->AddItem(aboutMenuItem);
+	
+	//layout menubar
+	AddItem(newThoughtMenuItem);
+	AddItem(editThoughtMenuItem);
+	AddItem(saveThoughtMenuItem);
+	AddItem(previewMenuItem);
+	AddItem(publishMenuItem);
+	AddItem(helpMenu);
+	
+	// set enabled/disabled triggers at initialization
+}
