de55b27e6105d344e0ee124f4e341ce324d2f31a
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-02-13 23:28:03 +0000

    got the layout right for saveidea
    git-svn-id: http://masterpiece.googlecode.com/svn@228 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/SaveIdea.cpp
+++ src/SaveIdea.cpp
@@ -1,7 +1,7 @@
 #include "SaveIdea.h"
 
 SaveIdea::SaveIdea(float mainX, float mainY, int currentID)
-	:	BWindow(BRect(0, 0, 308, 75), "Save As", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
+	:	BWindow(BRect(0, 0, 358, 60), "Save As", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
 {
 	BRect textFrame(0, 0, 300, 10);
 	titleText = new BTextView(textFrame, NULL, textFrame, B_FOLLOW_LEFT_RIGHT, B_WILL_DRAW);
@@ -16,12 +16,12 @@ SaveIdea::SaveIdea(float mainX, float mainY, int currentID)
 	
 	backView->SetLayout(new BGroupLayout(B_HORIZONTAL, 0.0));
 	backView->AddChild(BGridLayoutBuilder()
-		//.Add(titleString, 0, 0, 2, 1)
-		.Add(titleText, 0, 0, 2, 1)
-		//.Add(BSpaceLayoutItem::CreateVerticalStrut(1.0), 0, 1, 2, 1)
-		.Add(cancelButton, 0, 1)
-		.Add(saveButton, 1, 1)
-		.SetInsets(2, 2, 2, 2)
+		.Add(titleString, 0, 0, 2, 1)
+		.Add(titleText, 2, 0, 3, 1)
+		.Add(cancelButton, 3, 1)
+		.Add(saveButton, 4, 1)
+		.Add(BSpaceLayoutItem::CreateGlue(), 0, 2, 2, 1)
+		.SetInsets(2, 5, 2, 2)
 	);
 	MoveTo(mainX, mainY);
 }
@@ -29,6 +29,12 @@ void SaveIdea::MessageReceived(BMessage* msg)
 {
 	switch(msg->what)
 	{
+		case SAVE_IDEA:
+			this->Close();
+			break;
+		case CANCEL_SAVE:
+			this->Close();
+			break;
 		default:
 			BWindow::MessageReceived(msg);
 			break;
