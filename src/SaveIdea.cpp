32c1b59cf7f7f3739e0ce6691bc9811c9b69df9b
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-02-13 01:59:22 +0000

    trying to fix the height of the textframe, which is too tall... i think its layout related.. need to figure out how glue/strut work...
    git-svn-id: http://masterpiece.googlecode.com/svn@227 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/SaveIdea.cpp
+++ src/SaveIdea.cpp
@@ -1,10 +1,12 @@
 #include "SaveIdea.h"
 
 SaveIdea::SaveIdea(float mainX, float mainY, int currentID)
-	:	BWindow(BRect(0, 0, 400, 50), "Save As", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
+	:	BWindow(BRect(0, 0, 308, 75), "Save As", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
 {
-	BRect textFrame(0, 5, 300, 10);
-	titleText = new BTextView(textFrame, NULL, textFrame, B_FOLLOW_LEFT, B_WILL_DRAW);
+	BRect textFrame(0, 0, 300, 10);
+	titleText = new BTextView(textFrame, NULL, textFrame, B_FOLLOW_LEFT_RIGHT, B_WILL_DRAW);
+	titleText->MakeResizable(false);
+	titleText->SetWordWrap(false);
 	titleString = new BStringView(BRect(10, 10, 200, 30), NULL, "Enter Thought Title:");
 	saveButton = new BButton(BRect(190, 50, 270, 75), NULL, "Save", new BMessage(SAVE_IDEA));
 	cancelButton = new BButton(BRect(190, 50, 270, 75), NULL, "Cancel", new BMessage(CANCEL_SAVE));
@@ -16,9 +18,9 @@ SaveIdea::SaveIdea(float mainX, float mainY, int currentID)
 	backView->AddChild(BGridLayoutBuilder()
 		//.Add(titleString, 0, 0, 2, 1)
 		.Add(titleText, 0, 0, 2, 1)
-		.Add(BSpaceLayoutItem::CreateVerticalStrut(1.0), 0, 1, 2, 1)
-		.Add(cancelButton, 0, 2)
-		.Add(saveButton, 1, 2)
+		//.Add(BSpaceLayoutItem::CreateVerticalStrut(1.0), 0, 1, 2, 1)
+		.Add(cancelButton, 0, 1)
+		.Add(saveButton, 1, 1)
 		.SetInsets(2, 2, 2, 2)
 	);
 	MoveTo(mainX, mainY);
