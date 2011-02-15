6153810a657e53cf9e1323e3142aab03b35563e8
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-02-15 02:30:47 +0000

    think i got a good commonfunctions test.
    git-svn-id: http://masterpiece.googlecode.com/svn@234 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/CommonFunctions.cpp
+++ src/CommonFunctions.cpp
@@ -5,26 +5,30 @@ sqlite3* OpenSqliteDB()
 	char*		sqlErrMsg;
 	int			sqlValue;
 	sqlite3* 	opendb;
+	BString		tmpString;
+	ErrorAlert*	eAlert;
 	
 	sqlErrMsg = 0;
+	opendb = NULL;
 	app_info info;
 	be_app->GetAppInfo(&info);
 	BPath path(&info.ref);
 	path.GetParent(&path);
 	BString tmpPath = path.Path();
 	tmpPath += "/MasterPiece.db";
-	sqlValue = sqlite3_open_v2(tmpPath, &mpdb, SQLITE_OPEN_READWRITE, NULL); // open db
-	if(sqlite3_errcode(mpdb) == 14) // if error is SQLITE_CANTOPEN, then create db with structure
+	sqlValue = sqlite3_open_v2(tmpPath, &opendb, SQLITE_OPEN_READWRITE, NULL); // open db
+	if(sqlite3_errcode(opendb) == 14) // if error is SQLITE_CANTOPEN, then create db with structure
 	{
-		sqlValue = sqlite3_open_v2(tmpPath, &mpdb, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL); 
-		if(sqlite3_errcode(mpdb) == 0) // sqlite_ok
+		sqlValue = sqlite3_open_v2(tmpPath, &opendb, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL); 
+		if(sqlite3_errcode(opendb) == 0) // sqlite_ok
 		{
 			tmpString = "CREATE TABLE ideatable(ideaid integer primary key autoincrement, ideaname text, ideatext text, ismp integer, mpid integer, ordernumber integer);";
-			sqlValue = sqlite3_exec(mpdb, tmpString, NULL, NULL, &sqlErrMsg);
+			sqlValue = sqlite3_exec(opendb, tmpString, NULL, NULL, &sqlErrMsg);
 			if(sqlValue == SQLITE_OK) // if sql was successful
 			{
-				eAlert = new ErrorAlert("sql was created successfully");
-				eAlert->Launch();
+				//eAlert = new ErrorAlert("sql was created successfully");
+				//eAlert->Launch();
+				//return opendb;
 			}
 			else // sql not successful
 			{
@@ -34,48 +38,18 @@ sqlite3* OpenSqliteDB()
 		}
 		else // some kind of failure
 		{
-			eAlert = new ErrorAlert("1.0 Sql Error: ", sqlite3_errmsg(mpdb));
+			eAlert = new ErrorAlert("1.0 Sql Error: ", sqlite3_errmsg(opendb));
 			eAlert->Launch();
 		}
 	}
-	else if(sqlite3_errcode(mpdb) == 0) // sqlite_OK, it exists
+	else if(sqlite3_errcode(opendb) == 0) // sqlite_OK, it exists
 	{
-		sqlValue = sqlite3_prepare_v2(mpdb, "select ideaname, ideaid from ideatable where ismp = 1", -1, &ideaStatement, NULL);
-		if(sqlValue == SQLITE_OK) // sql query was successful
-		{
-			while(sqlite3_step(ideaStatement) == SQLITE_ROW)
-			{
-				tmpString = sqlite3_mprintf("%s", sqlite3_column_text(ideaStatement, 0));
-				openMasterpieceListView->AddItem(new MPStringItem(tmpString, sqlite3_column_int(ideaStatement, 1)));
-			}
-			openMasterpieceListView->SetInvocationMessage(new BMessage(OPEN_EXISTING_MP));
-		}
-		else // sql select failed
-		{
-			eAlert = new ErrorAlert("No Masterpiece Exist. Please Create One First.");
-			eAlert->Launch();
-		}
-		sqlValue = sqlite3_prepare_v2(mpdb, "select ideaname, ideaid from ideatable where ismp = 0", -1, &ideaStatement, NULL);
-		if(sqlValue == SQLITE_OK) // sql statement was prepared
-		{
-			while(sqlite3_step(ideaStatement) == SQLITE_ROW)
-			{
-				tmpString = sqlite3_mprintf("%s", sqlite3_column_text(ideaStatement, 0));
-				openThoughtListView->AddItem(new MPStringItem(tmpString, sqlite3_column_int(ideaStatement, 1)));
-			}
-			openThoughtListView->SetInvocationMessage(new BMessage(OPEN_EXISTING_THT));
-		}
-		else
-		{
-			eAlert = new ErrorAlert("No Thoughts Exist.  Please Create One First.");
-			eAlert->Launch();
-		}
-		sqlite3_finalize(ideaStatement);
+		//return opendb;
 	}
 	else // if error is not ok or not existing, then display error in alert
 	{
-		eAlert = new ErrorAlert("1.2 Sql Error: ", sqlite3_errmsg(mpdb));
+		eAlert = new ErrorAlert("1.2 Sql Error: ", sqlite3_errmsg(opendb));
 		eAlert->Launch();
-	}	
+	}
 	return opendb;
 }
--- src/CommonFunctions.h
+++ src/CommonFunctions.h
@@ -2,6 +2,12 @@
 #define COMMONFUNCTIONS_H
 
 #include <sqlite3.h>
+#include <Application.h>
+#include <Roster.h>
+#include <Path.h>
+#include <String.h>
+
+#include "ErrorAlert.h"
 
 sqlite3* OpenSqliteDB(void);
 
