f313bc2c2693cd6575633cf06fbed5154a2844a2
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-03-08 02:20:25 +0000

    working on replacement for array stuff
    git-svn-id: http://masterpiece.googlecode.com/svn@289 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/IdeaStringItem.cpp
+++ src/IdeaStringItem.cpp
@@ -5,7 +5,37 @@ IdeaStringItem::IdeaStringItem(BString itemText, int ideaid)
 {
 	ideaID = ideaid;
 }
+IdeaStringItem::IdeaStringItem(BString ideaname, int ideaid, BString ideatext, int ismp, int mpid, int ordernumber)
+	:	BStringItem(ideaname)
+{
+	ideaID = ideaid;
+	isMP = ismp;
+	mpID = mpid;
+	orderNumber = ordernumber;
+	ideaText = ideatext;
+	ideaName = ideaname;
+}
 int IdeaStringItem::ReturnID(void) const
 {
 	return ideaID;
+}
+int IdeaStringItem::ReturnIsMP(void) const
+{
+	return isMP;
+}
+int IdeaStringItem::ReturnMpID(void) const
+{
+	return mpID;
+}
+int IdeaStringItem::ReturnOrderNumber(void) const
+{
+	return orderNumber;
+}
+BString IdeaStringItem::ReturnText(void) const
+{
+	return ideaText;
+}
+BString IdeaStringItem::ReturnName(void) const
+{
+	return ideaName;
 }
--- src/IdeaStringItem.h
+++ src/IdeaStringItem.h
@@ -8,9 +8,20 @@ class IdeaStringItem : public BStringItem
 {
 	public:
 					IdeaStringItem(BString itemText, int ideaid = -1);
+					IdeaStringItem(BString ideaname, int ideaid = -1, BString ideatext, int ismp, int mpid, int ordernumber);
 				int	ReturnID(void) const;
+				int ReturnIsMP(void) const;
+				int ReturnMpID(void) const;
+				int ReturnOrderNumber(void) const;
+				BString ReturnText(void) const;
+				BString ReturnName(void) const;
 	private:
-				int ideaID;
+				int 	ideaID;
+				int		isMP;
+				int		mpID;
+				int		orderNumber;
+				BString ideaText;
+				BString ideaName;
 };
 
 
