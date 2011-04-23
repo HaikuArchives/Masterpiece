c2bdf0d101fa682f4ff2840031ac9c6acc0ddd17
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-04-23 00:46:40 +0000

    working sqlwrapper, i think i got it started well...
    git-svn-id: http://masterpiece.googlecode.com/svn@333 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/CommonFunctions.h
+++ src/CommonFunctions.h
@@ -15,4 +15,17 @@ int			BindInteger(sqlite3_stmt* tmpStatement, int bindPlace, int bindValue, cons
 int			StepSql(sqlite3_stmt* tmpStatement, const char* errorNumber);
 int			RunSql(sqlite3* tmpdb, const char* sqlquery, sqlite3_stmt** tmpStatement, const char* errorNumber, int bindCount, int tmpInt=-1, const char** unused=NULL);
 
+class SqlObject
+{
+	public:
+								SqlObject(sqlite3* sqlDB, sqlite3_stmt** sqlStatement);
+								~SqlObject(void);
+				int				PrepareSql(void);
+				int				BindInt(void);
+				int				StepSql(void);
+				int				ResetSql(void);
+	private:
+				sqlite3*		sqldb;
+				sqlite3_stmt**	sqlstatement;
+};
 #endif
