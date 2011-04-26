06feb6181d23a2d0efb1190fd941cdc62ffe9f23
pjrinaldi <pjrinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-04-26 01:34:48 +0000

    still working on bind blob
    git-svn-id: http://masterpiece.googlecode.com/svn@348 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/CommonFunctions.h
+++ src/CommonFunctions.h
@@ -21,7 +21,7 @@ class SqlObject
 				void			BindValue(int bindPlace, double bindValue);
 				void			BindValue(int bindPlace, int64 bindValue);
 				void			BindValue(int bindPlace, const char* bindValue);
-				void			BindValue(int bindPlace, 
+				//void			BindValue(int bindPlace, // blob bind
 				void			BindValue(int bindPlace);
 				int				ReturnValue(int returnPlace);
 				void			StepSql(void);
