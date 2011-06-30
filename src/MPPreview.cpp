ea7fc76011f82fd0541526039aa34da479bc05b0
haikurinaldi <haikurinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-06-30 17:22:14 +0000

    working on the MPPreview window which will probably end up being a custom generated preview window from the thoughts into their corresponding stylable text calls... but we'll see how good i am at this stuff.
    git-svn-id: http://masterpiece.googlecode.com/svn@429 f1780a09-8134-7589-23cd-ad4540f9662e

---
+++ src/MPPreview.cpp
@@ -0,0 +1,121 @@
+#include "MPPreview.h"
+
+MPPreview::MPPreview(const BMessage &msg, const BMessenger &msgr, BString windowTitle, int ideaID)
+	:	BWindow(BRect(100, 100, 900, 700), windowTitle, B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS | B_AUTO_UPDATE_SIZE_LIMITS, B_CURRENT_WORKSPACE)
+{
+	// initialize controls
+	BRect r = Bounds();
+	r.bottom = r.bottom - 50;
+	previewTextView = new BTextView(r, NULL, r, B_FOLLOW_ALL, B_WILL_DRAW);	
+	backView = new BView(Bounds(), "backview", B_FOLLOW_ALL, B_WILL_DRAW);
+	backView->SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR));
+	AddChild(backView);
+	// gui layout builder
+	backView->SetLayout(new BGroupLayout(B_HORIZONTAL, 0.0));
+	backView->AddChild(BGridLayoutBuilder()
+		//.Add(new EditorMenu(), 0, 0)
+		.Add(new BScrollView("scroll_editor", previewTextView, B_FOLLOW_ALL_SIDES, 0, false, true, B_FANCY_BORDER), 0, 0)
+		.SetInsets(0, 0, 0, 0)
+	);
+	/*
+	currentideaID = ideaID; // pass current idea id selected to editor window for use
+	if(currentideaID != -1) // if id has a real value
+	{
+		sqlObject = new SqlObject(ideaStatement, "7");
+		sqlObject->PrepareSql("select ideatext from ideatable where ideaid = ?");
+		sqlObject->BindValue(1, currentideaID);
+		sqlObject->StepSql();
+		editorTextView->SetText(sqlObject->ReturnText(0));
+		sqlObject->FinalizeSql();
+		sqlObject->CloseSql();
+	}
+	*/
+}
+void MPPreview::MessageReceived(BMessage* msg)
+{
+	//BRect r(Bounds());
+	switch(msg->what)
+	{
+		/*
+		case MENU_NEW_THT: // open another untitled editor window
+			tmpEditor = new MPPreview(BMessage(SHOW_LAUNCHER), BMessenger(this), "MasterPiece Editor - untitled", -1);
+			tmpEditor->Show();
+			break;
+		case MENU_EDT_THT: // edit current idea name for editing
+			xPos = (r.right - r.left) / 2; // find xpos for window
+			yPos = (r.bottom - r.top) / 2; // find ypos for window
+			editIdeaName = new EditIdeaName(BMessage(UPDATE_TITLE), BMessenger(this), xPos, yPos, currentideaID);
+			editIdeaName->Show(); // show edit idea name window
+			break;
+		case MENU_SAV_THT: // save current idea progress
+			if(currentideaID == -1) // if its untitled insert new thought, then show saveidea to apply a name...
+			{
+				sqlObject = new SqlObject(ideaStatement, "8");
+				sqlObject->PrepareSql("insert into ideatable (ideaname, ideatext, ismp) values('untitled', ?, 0)");
+				sqlObject->BindValue(1, editorTextView->Text());
+				sqlObject->StepSql();
+				xPos = (r.right - r.left) / 2; // find xpos for window
+				yPos = (r.bottom - r.top) / 2; // find ypos for window
+				saveIdea = new SaveIdea(BMessage(UPDATE_TITLE), BMessenger(this), xPos, yPos, sqlObject->ReturnLastInsertRowID());
+				currentideaID = sqlObject->ReturnLastInsertRowID();
+				sqlObject->FinalizeSql();
+				sqlObject->CloseSql();
+				saveIdea->Show(); // show save window to name the untitled thought
+			}
+			else // already exists, just update ideatext and save new information
+			{
+				sqlObject = new SqlObject(ideaStatement, "9");
+				sqlObject->PrepareSql("update ideatable set ideatext = ? where ideaid = ?");
+				sqlObject->BindValue(1, editorTextView->Text());
+				sqlObject->BindValue(2, currentideaID);
+				sqlObject->StepSql();
+				sqlObject->FinalizeSql();
+				sqlObject->CloseSql();
+			}
+			break;
+		case MENU_PRV_THT: // preview thought in html in webpositive
+			printf("save data to tmp file, export to python html one and open data in preview window or webpositive");
+			break;
+		case MENU_PUB_THT: // publish thought by opening publish window
+			printf("save data, open publish to window, export to python and save as name in publish window");
+			break;
+		case MENU_HLP_THT: // open help topic window
+			printf("open help topic window");
+			break;
+		case MENU_KEY_THT: // open keyboard reference window
+			printf("open keyboard reference window");
+			break;
+		case MENU_MRK_THT: // open markup reference window
+			printf("open markup reference window");
+			break;
+		case MENU_ABT_THT: // open about window
+			printf("open about window");
+			break;
+		case UPDATE_TITLE: // update title with the name from the saveidea window
+			if(msg->FindString("updatetitle", &updateTitle) == B_OK) // updated title exists in variable
+			{
+				tmpString = "Masterpiece Editor - ";
+				tmpString += updateTitle;
+				this->SetTitle(tmpString);
+			}
+			else // 
+			{
+				eAlert = new ErrorAlert("3.1 Editor Error: Message not found."); // message variable not found
+			}
+			break;
+			*/
+		default:
+		{
+			BWindow::MessageReceived(msg);
+			break;
+		}
+	}
+}
+bool MPPreview::QuitRequested(void)
+{
+	// on quit, show launcher by sending message
+	//launcherMessage.MakeEmpty();
+	//launcherMessage.AddInt64("showLauncher", 1);
+	//launcherMessenger.SendMessage(&launcherMessage);
+	return true;
+}
---
+++ src/MPPreview.h
@@ -0,0 +1,52 @@
+#ifndef MPPREVIEW_H
+#define MPPREVIEW_H
+
+#include <Window.h>
+#include <View.h>
+#include <sqlite3.h>
+#include <Application.h>
+#include <String.h>
+#include <ScrollView.h>
+#include <Messenger.h>
+#include <Message.h>
+#include <stdio.h>
+#include <GroupLayout.h>
+#include <SpaceLayoutItem.h>
+#include <GridLayoutBuilder.h>
+#include <TextView.h>
+#include <ScrollView.h>
+
+#include "ErrorAlert.h"
+#include "CommonFunctions.h"
+
+//#define SHOW_LAUNCHER		'lnch' // show mp launcher window
+//#define UPDATE_TITLE		'uttl' // update title...
+
+class MPPreview : public BWindow
+{
+public:
+						MPPreview(const BMessage &msg, const BMessenger &msgr, BString windowTitle, int ideaID);
+		void			MessageReceived(BMessage* msg);
+		bool			QuitRequested(void);
+private:
+		BView*			backView;
+		//BMessage		launcherMessage;
+		//BMessenger		launcherMessenger;
+		BTextView*		previewTextView;
+		//MPEditor*		tmpEditor;
+		ErrorAlert*		eAlert;
+		//SaveIdea*		saveIdea;
+		//EditIdeaName*	editIdeaName;
+		sqlite3*		mpdb;
+		sqlite3_stmt*	ideaStatement;
+		char*			sqlErrMsg;
+		BString			tmpString;
+		//BString			updateTitle;
+		int				sqlValue;
+		int				currentideaID;
+		float			xPos;
+		float			yPos;
+		SqlObject*		sqlObject;
+};
+
+#endif
