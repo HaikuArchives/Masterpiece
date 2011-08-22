a7fdf7f0d5e3ac02c59147ab845b7c9154b2136e
haikurinaldi@gmail.com <haikurinaldi@gmail.com@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-08-22 23:15:38 +0000

    working on custom publish file menuitem, then to update my menuitems for mpbuilder accordingly to put it back to what it was
    git-svn-id: http://masterpiece.googlecode.com/svn@501 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/PublishFilePanel.h
+++ src/PublishFilePanel.h
@@ -1,6 +1,14 @@
 #ifndef PUBLISHFILEPANEL_H
 #define PUBLISHFILEPANEL_H
 
-
+class PublishFilePanel : public BFilePanel
+{
+	public:
+		PublishFilePanel(BMessenger* target, BRefFilter* filter=NULL);
+		void SelectionChanged();
+		
+	private:
+		// popupmenu item as a dropdown selector
+};
 
 #endif
