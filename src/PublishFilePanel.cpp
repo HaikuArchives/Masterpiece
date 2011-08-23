0c53931d4b74bd5ef83464d9fadd7c80f4af2759
haikurinaldi <haikurinaldi@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-08-23 20:33:55 +0000

    code doesn't work right now, but basics are there to interpret and apply to my needs
    git-svn-id: http://masterpiece.googlecode.com/svn@502 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/PublishFilePanel.cpp
+++ src/PublishFilePanel.cpp
@@ -1,2 +1,81 @@
 #include "PublishFilePanel.h"
 
+PublishFilePanel::PublishFilePanel(BMessenger* target, BRefFilter* filter)
+	:	BFilePanel(B_OPEN_PANEL, target, NULL, B_DIRECTORY_NODE, false, NULL, filter)
+{
+	BWindow *w;
+	w = Window();
+	if(w->Lock())
+	{
+		BRect btnrect;
+		BView* v = w->ChildAt(0);
+		BView *cancelBtn = v->FindView("cancel button");
+		
+		if(cancelBtn)
+		{
+			BView* parentview;
+			float charWidth;
+			
+			charWidth = cancelBtn->StringWidth("Select Current Directory");
+			btnrect = cancelBtn->Frame();
+			btnrect.right = btnrect.left - 10;
+			btnrect.left = btnrect.right - charWidth - 40;
+			fCurrentDirBtn = new BButton(btnrect, "current dir button", "Select Current Directory", NULL, B_FOLLOW_RIGHT | B_FOLLOW_BOTTOM);
+			
+			fCurrentDirBtn->SetTarget(*target);
+			
+			parentview = cancelBtn->Parent();
+			parentview->AddChild(fCurrentDirBtn);
+			
+			entry_ref ref;
+			BMessage* msg = new BMessage('slct');
+			GetPanelDirectory(&ref);
+			msg->AddRef("refs", &ref);
+			fCurrentDirBtn->SetMessage(msg);
+		}
+		
+		w->Unlock();
+	}
+}
+
+void PublishFilePanel::SelectionChanged()
+{
+  BWindow *wind;
+
+  wind = Window();
+
+  if (wind->Lock()) {
+    entry_ref currRef;
+
+    GetPanelDirectory(&currRef);
+
+    //  modify the btn's msg
+
+    BMessage *msg = new BMessage('slct');
+    msg->AddRef("refs",&currRef);
+    fCurrentDirBtn->SetMessage(msg);
+
+    wind->Unlock();
+  }
+}
+
+class TDirFilter : public BRefFilter {
+
+public:
+  bool Filter(const entry_ref* e, BNode* n, struct stat* s,
+              const char* mimetype);
+};
+
+bool
+TDirFilter::Filter(const entry_ref* e, BNode* n,
+                   struct stat* s, const char* mimetype)
+{
+  if (strcmp("application/x-vnd.Be-directory",
+             mimetype) == 0)
+    return true;
+  else if (strcmp("application/x-vnd.Be-volume",
+                  mimetype) == 0)
+    return true;
+  else
+    return false;
+}
--- src/PublishFilePanel.h
+++ src/PublishFilePanel.h
@@ -1,6 +1,13 @@
 #ifndef PUBLISHFILEPANEL_H
 #define PUBLISHFILEPANEL_H
 
+#include <Message.h>
+#include <Messenger.h>
+#include <FilePanel.h>
+#include <Button.h>
+#include <Window.h>
+#include <View.h>
+
 class PublishFilePanel : public BFilePanel
 {
 	public:
@@ -9,6 +16,7 @@ class PublishFilePanel : public BFilePanel
 		
 	private:
 		// popupmenu item as a dropdown selector
+		BButton* fCurrentDirBtn;
 };
 
 #endif
