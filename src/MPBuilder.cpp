9785a59bf85035e997ef96b83851cb760c06c421
haikurinaldi@gmail.com <haikurinaldi@gmail.com@f1780a09-8134-7589-23cd-ad4540f9662e>
2011-09-01 01:53:36 +0000

    working on getting it to copy the damn file to another directory
    git-svn-id: http://masterpiece.googlecode.com/svn@538 f1780a09-8134-7589-23cd-ad4540f9662e

--- src/MPBuilder.cpp
+++ src/MPBuilder.cpp
@@ -257,21 +257,22 @@ void MPBuilder::MessageReceived(BMessage* msg)
 				printf(publishPath);
 				//entry.Unset();
 				publishFile.SetTo(tmpPath);
-				publishFile.Rename(publishPath, true);
+				//publishFile.Rename(publishPath, true);
 				printf("Tmp Path: %s\nPublishPath: %s\n", tmpPath.String(), publishPath.String());
 				//entry.SetTo(&ref);
 				//entry.SetTo(&ref);
 				//entry.GetPath(&path);
-				if(publishDirectory.SetTo(&ref) == B_OK)
+				if(publishDirectory.SetTo() == B_OK)
 				{
+					//printf("publishdirectory %s\n", path.Path());
 					printf("successful directory set\n");
 					publishFile.MoveTo(&publishDirectory, publishPath, true);
+					//publishFile.Rename(publishPath, true);
 				}
 				else
 				{
 					printf("directory set failed");
 				}
-				//publishDirectory->Unset();
 			}
 			// clean up the temporary files...
 			break;
